2b549071034ccdc6423827eee379c98920dc172b
shiba <shiba@taobao.com>
2013-10-30 12:22:51 +0800

    local submit

--- testcase/gallery/wkeditor1.0.js
+++ testcase/gallery/wkeditor1.0.js
@@ -1,29 +1,26 @@
-
-
 var S = KISSY;
 var editor = new S.WKeditor({
-	ele:"#WKeditor",
-	message:'<div class="WKeditor_message"><h3>请输入正文</h3><p>小提示：<br>\
+    ele:"#WKeditor",
+    message:'<div class="WKeditor_message"><h3>请输入正文</h3><p>小提示：<br>\
                 1. 鼠标选中已输入文字可修改文字样式。<br>\
                 2. 支持粘帖截图(chrome firefox)。<br>\
                 3. 移动到未居中的图片上出现居中按钮。<br></div>',
-	font:[
-		"normalFont","largeFont","hugeFont","strongFont","listText"
-	]
+    font:[
+        "normalFont","largeFont","hugeFont","strongFont","listText"
+    ]
 });
 editor.init();
-console.log("初始化完成");
-console.log("装载按钮 “红”");
+console.log('初始化"normalFont","largeFont","hugeFont","strongFont","listText"完成');
+console.log("装载自定义按钮 “红”");
 editor.addFont({name:"FontColor",command:"FontColor",title:"设置红色",value:"红"},function(){
     console.log("点击----> 红");
 });
 editor.plug({name:"image",title:"插入图片",value:"图"},function(){
     console.log("点击----> 图");
 });
-console.log("装载按钮 “图”");
+console.log("装载自定义按钮 “图”");
 editor.plug({name:"my",title:"自定义模版",value:"模"},function(){
     console.log("点击----> 模")
 });
-console.log("装载按钮 “模”");
+console.log("装载自定义按钮 “模”");
 console.log("更多API：http://gallery.kissyui.com/WKeditor/1.0/guide/index.html");
-
--- testcase/gallery/wkeditor1.0.php
+++ testcase/gallery/wkeditor1.0.php
@@ -5,10 +5,8 @@
     需要测试的dom结构，注意，最外层<div class="J_TScriptedModule" data-componentid="uniqueSign"> 的class和为属性不可修改
     用户的javascript理论上只可以作用到这个dom下面，不可以"越界"
 -->
-
 <script type="text/javascript" src="http://a.tbcdn.cn/s/kissy/1.3.0/kissy.js"></script>
 <link rel="stylesheet" href="http://g.tbcdn.cn/kissy/k/1.3.0/css/dpl/base-min.css">
-
 <style type="text/css">
     body{
         background: #eee;
@@ -17,11 +15,13 @@
         padding: 0px;
     }
     #wankePost {
-        width: 60%;
+        width: 700px;
         margin: auto;
         background: #fff;
         padding: 10px;
         border-radius: 5px;
+        position: relative;
+
     }
     .title{
         font-size: 24px;
@@ -45,19 +45,11 @@
     .top .bb{
         width:800px;margin:auto
     }
-
-    #WKeditor .ForeColor{
-        background: #333;
-        font-weight: bold;
-    }
-    #WKeditor .ForeColor:hover{
-
     #WKeditor .FontColor{
         background: #333;
         font-weight: bold;
     }
     #WKeditor .FontColor:hover{
-
         background: #141414;
     }
     .list_color{
@@ -88,20 +80,20 @@
     .link:hover{
         color: #fff;
     }
-
+    .mini{
+        font-size: 12px;
+    }
 </style>
-
 <div class="J_TScriptedModule" data-componentid="uniqueSign">
-  <h2 class="top">
-      <div class="bb">WKeditor 1.0 <a class="link" href="http://gallery.kissyui.com/WKeditor/1.0/guide/index.html" target="_blank">API：http://gallery.kissyui.com/WKeditor/1.0/guide/index.html</a></div>
+    <h2 class="top">
+        <div class="bb">WKeditor 1.0 <a class="link" href="http://gallery.kissyui.com/WKeditor/1.0/guide/index.html" target="_blank">API：http://gallery.kissyui.com/WKeditor/1.0/guide/index.html</a></div>
 
-  </h2>
+    </h2>
 
-  <div id="wankePost">
-      <div class='title'>标题 <span class='mini'>（建议开启控制台查看console.log）</span></div>    
-      <div id="WKeditor"></div>
-  </div>
-
+    <div id="wankePost">
+        <div class='title'>标题 <span class='mini'>（建议开启控制台查看console.log）</span></div>
+        <div id="WKeditor"></div>
+    </div>
 
 </div>
 
@@ -112,18 +104,12 @@
         modules:"openjs/kissy/gallery/wkeditor/1.0/index"
     }
 
-
 </script>
 
-</script>   
-
-
-<!--这里是将自己的js让服务端编译一下, 配置下服务端的php路径和自己的js即可, 注意路径-->
+<!--这里是将自己的js让服务端编译一下，配置下服务端的php路径和自己的js即可，注意路径-->
 <?php
 $jsurl="testcase/gallery/wkeditor1.0.js";//注意路径
 $jsservice="../common/cajoled_service.php";//注意路径
 
-
 include("../common/foot.php");//引入foot
 ?>
-
