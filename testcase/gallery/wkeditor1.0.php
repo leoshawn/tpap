2985208ac06cd1b47acfa4600f0f9d831f343ebe
changyuan.lcy <changyuan.lcy@ali-64843n.taobao.ali.com>
2013-10-30 11:34:20 +0800

    testcase css style

--- testcase/gallery/wkeditor1.0.php
+++ testcase/gallery/wkeditor1.0.php
@@ -15,11 +15,15 @@
         padding: 0px;
     }
     #wankePost {
-        width: 60%;
+        width: 700px;
         margin: auto;
         background: #fff;
         padding: 10px;
         border-radius: 5px;
+        border: 1px solid green;
+        _left:20%;
+        position: relative;
+
     }
     .title{
         font-size: 24px;
@@ -80,6 +84,14 @@
     }
     .mini{
         font-size: 12px;
+    }
+    .div{
+        display: block!important;
+        text-align: center;
+    }
+    .J_TScriptedModule{
+        border: 1px solid green;
+
     }
 </style>
 <div class="J_TScriptedModule" data-componentid="uniqueSign">
